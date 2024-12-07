ed3d9696eafad03cbab2189ddcfd36d192f3dc23
Jonathan <143897103+JonathanMM03@users.noreply.github.com>
2024-12-07 08:33:43 -0600

    Credenciales de AWS

--- config/environment.js
+++ config/environment.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 3001,
-  DB_NAME: process.env.DB_NAME || 'Don_Galleto',
-  DB_USER: process.env.DB_USER || 'root',
-  DB_PASS: process.env.DB_PASS || '',
-  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_NAME: process.env.DB_NAME || 'http://galleto.c3mkc6auc4br.us-east-2.rds.amazonaws.com/',//'Don_Galleto',
+  DB_USER: process.env.DB_USER || 'admin',
+  DB_PASS: process.env.DB_PASS || 'rootgalleto',
+  DB_HOST: process.env.DB_HOST || 'Don_Galleto',
 };
